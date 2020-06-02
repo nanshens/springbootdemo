f3d696c43fd06b4e0ce130dfeb0e7a26bb990ea2
nanshens <619232906@qq.com>
2020-06-02 20:50:17 +0800

    delete core

--- spring-data-jpa/src/test/java/com/ns/springdatajpa/SpringDataJpaApplicationTests.java
+++ spring-data-jpa/src/test/java/com/ns/springdatajpa/SpringDataJpaApplicationTests.java
@@ -2,6 +2,9 @@ package com.ns.springdatajpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
